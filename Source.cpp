a91086125caee70417990cf276ab283179d073b6
Soumodeep Dutta <duttasoumo@gmail.com>
2021-05-22 01:38:07 +0530

    first commit

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a, b;
+	cout << "\nENter a num :";
+	cin >> a;
+	cout << "\nENter another num : ";
+	cin >> b;
+	cout << "\nSUM : " << (a + b);
+	system("pause");
+	return 0;
+}
